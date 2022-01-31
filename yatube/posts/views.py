e1f73386d7305e996b378a2a5826937f07ae9b75
Сергей Чувычкин <chuvichkin@yandex.ru>
2022-01-31 16:18:41 +0300

    3 спринт

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,25 +1,18 @@
-#from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
+
 
 def index(request):
     text = 'Последние обновления на сайте'
     posts = Post.objects.order_by('-pub_date')[:10]
     # В словаре context отправляем информацию в шаблон
-    context = {'posts': posts, 'text':text}
+    context = {'posts': posts, 'text': text}
     template = 'posts/index.html'
     return render(request, template, context)
 
-def group_posts(request, slug):
-    # Функция get_object_or_404 получает по заданным критериям объект 
-    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
-    # В нашем случае в переменную group будут переданы объекты модели Group,
-    # поле slug у которых соответствует значению slug в запросе
-    group = get_object_or_404(Group, slug=slug)
 
-    # Метод .filter позволяет ограничить поиск по критериям.
-    # Это аналог добавления
-    # условия WHERE group_id = {group_id}
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'group': group,
