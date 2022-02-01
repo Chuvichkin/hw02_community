1d538f404ebfa093ced405a1312212a0f22c889c
Сергей Чувычкин <chuvichkin@yandex.ru>
2022-02-01 20:05:41 +0300

    3 спринт_3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
-num_of_posts = 10
+NUM_OF_POSTS = 10
 
 
 def index(request):
     text = 'Последние обновления на сайте'
-    posts = Post.objects.all()[:num_of_posts]
+    posts = Post.objects.all()[:NUM_OF_POSTS]
     context = {'posts': posts, 'text': text}
     template = 'posts/index.html'
     return render(request, template, context)
@@ -13,7 +13,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:num_of_posts]
+    posts = group.posts.all()[:NUM_OF_POSTS]
     context = {
         'group': group,
         'posts': posts,
